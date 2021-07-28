80873881608849bc887e58d3e21a6d6a5ca0bdc2
Tran Nguyen <trannguyen61st@gmail.com>
2021-07-28 21:17:11 +0700

    Chap 4 + 5

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+# you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module RailsTraining
@@ -12,7 +12,7 @@ module RailsTraining
     config.load_defaults 6.1
 
     config.i18n.load_path += Dir[
-      Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
+      Rails.root.join("config", "locales", "**", "*.{rb,yml}")
     ]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
