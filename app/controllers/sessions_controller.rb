7228ce552a7294f0664b6d656fcf7d28b20f6109
Tran Nguyen <trannguyen61st@gmail.com>
2021-08-02 08:32:01 +0700

    Chapter 10: Update user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,6 +22,6 @@ class SessionsController < ApplicationController
   def handle_login user, remember_me
     log_in user
     remember_me == "1" ? remember(user) : forget(user)
-    redirect_to user
+    redirect_back_or user
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: Settings.user.admin.name,
+            email: Settings.user.admin.email,
+            password: Settings.user.admin.password,
+            password_confirmation: Settings.user.admin.password,
+            admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "#{Settings.user.faker.email1}#{n+1}#{Settings.user.faker.email2}"
+  User.create!(name: name,
+              email: email,
+              password: Settings.user.faker.password,
+              password_confirmation: Settings.user.faker.password)
+end
